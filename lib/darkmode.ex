e82bae1107c22b352a1db2b0c947815d5926c89d
sven-zo <svenzo@outlook.com>
2019-03-26 23:36:46 +0100

    initial commit

---
+++ lib/darkmode.ex
@@ -0,0 +1,18 @@
+defmodule Darkmode do
+  @moduledoc """
+  Documentation for Darkmode.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Darkmode.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/darkmode_test.exs
@@ -0,0 +1,8 @@
+defmodule DarkmodeTest do
+  use ExUnit.Case
+  doctest Darkmode
+
+  test "greets the world" do
+    assert Darkmode.hello() == :world
+  end
+end
