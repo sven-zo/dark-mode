02dabb283f5a4bc0c9085eb37372902ecfcd8d14
sven-zo <svenzo@outlook.com>
2019-05-22 16:24:34 +0200

    ci: update auth-and-push

--- .github/auth-and-push/entrypoint.sh
+++ .github/auth-and-push/entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 echo "$GCLOUD_AUTH" | base64 -d > "$HOME"/gcloud.json
-sh "gcloud auth activate-service-account --key-file=$HOME/gcloud.json"
+sh -c "gcloud auth activate-service-account --key-file=$HOME/gcloud.json"
 
 sh -c "docker $*"
