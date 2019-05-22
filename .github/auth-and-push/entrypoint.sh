170f39f11ca4889abcca158cad6cf10a1ee014de
sven-zo <svenzo@outlook.com>
2019-05-22 16:15:04 +0200

    ci: fix base64 decode

--- .github/auth-and-push/entrypoint.sh
+++ .github/auth-and-push/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
+echo "$GCLOUD_AUTH" | base64 -d > "$HOME"/gcloud.json
 sh -c "gcloud auth activate-service-account --key-file=$HOME/gcloud.json $*"
 
 sh -c "docker $*"
