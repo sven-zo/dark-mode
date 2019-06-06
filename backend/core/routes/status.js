c0b318a105b0dabab0786d0b514095142f1be1ac
sven-zo <svenzo@outlook.com>
2019-06-06 12:41:41 +0200

    backend/core: add status route

---
+++ backend/core/routes/status.js
@@ -0,0 +1,33 @@
+// Environment variables
+require('dotenv').config()
+const { HOSTNAME_URL } = process.env
+// Dependencies
+const { send } = require('micro')
+const request = require('../utils/request')
+
+// Route
+module.exports = async (_req, res) => {
+  let didTimeout = false
+  let check = false
+  try {
+    const timer = setTimeout(() => {
+      didTimeout = true
+      send(res, 200, { services: { deepmoji: false, core: true } })
+    }, 5000)
+    const reponse = await request({
+      hostname: HOSTNAME_URL,
+      path: '/',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    clearTimeout(timer)
+    check = reponse === 'Service is active'
+    if (!didTimeout) {
+      send(res, 200, { services: { deepmoji: check, core: true } })
+    }
+  } catch (err) {
+    send(res, 500, { error: err })
+  }
+}
