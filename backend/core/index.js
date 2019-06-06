d767b0685b6aa7a84e7da8eb8f5921c882028de8
sven-zo <svenzo@outlook.com>
2019-06-06 12:42:01 +0200

    backend/core: add router

--- backend/core/index.js
+++ backend/core/index.js
@@ -1,21 +1,13 @@
-require('dotenv').config()
-const request = require('./request')
-const { HOSTNAME_URL } = process.env
+// Dependencies
+const { router, get } = require('microrouter')
+// Routes
+const index = require('./routes')
+const status = require('./routes/status')
+const notfound = require('./routes/notfound')
 
-module.exports = async (_req, res) => {
-  let check = false
-  try {
-    const d = await request({
-      hostname: HOSTNAME_URL,
-      path: '/',
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    check = d === 'Service is active'
-    res.end(`deepmoji==${check}`)
-  } catch (err) {
-    res.end(err)
-  }
-}
+// Router
+module.exports = router(
+  get('/', index),
+  get('/status', status),
+  get('/*', notfound)
+)
