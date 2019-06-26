0d867ab1a7c71bb5637736a00395bf8fc6edc069
sven-zo <svenzo@outlook.com>
2019-06-26 15:48:12 +0200

    frontend/demo: initial commit

---
+++ demo-frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="h-screen w-screen bg-cover">
+    <dark-mode-icon></dark-mode-icon>
+  </div>
+</template>
+
+<script>
+import DarkModeIcon from './components/DarkModeIcon.vue'
+
+export default {
+  name: 'app',
+  components: {
+    DarkModeIcon
+  }
+}
+</script>
+
+<style>
+@import 'assets/styles/tailwind.postcss';
+
+#app {
+  background-image: url('/img/star-bg.jpg');
+}
+</style>
